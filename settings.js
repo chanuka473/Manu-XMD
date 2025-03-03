bbaaf75271d596aac8f3e22d0af86ad059301448
chanuka473 <chanukaudara1000@gmail.com>
2025-03-03 23:51:52 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -43,7 +43,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'Manu-MD&dXdgSTTQ#dmNgb34HrfRghmzIGV5ZQM-m63Z3Np554oHx-381BHA' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'Manu-MD&5aMChToT#-zMnGaUxiM4efF28wqr6pnWyxJnb0_K_1v4bnE6aPeU' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SUDO: process.env.SUDO === undefined ? '94742274855' : process.env.SUDO,
